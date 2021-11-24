8427e38937ae0ce26009be3e50bc90f5b17b50fc
Mahmoud Mostafa <mah@moud.info>
2021-11-24 21:26:21 +0000

    Add functional test for valid input

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,20 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+  test('Convert a valid input such as 10L', function (done) {
+    chai
+      .request(server)
+      .get('/api/convert?input=10L')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          initNum: 10,
+          initUnit: 'L',
+          returnNum: 2.64172,
+          returnUnit: 'gal',
+          string: '10 litres converts to 2.64172 gallons',
+        });
+        done();
+      });
+  });
 });
